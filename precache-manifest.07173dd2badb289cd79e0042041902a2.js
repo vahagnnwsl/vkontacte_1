5ec04443f7ef9c499d695c17fb468e7a7c5bb124
Anna <hakobyananna0105@gmail.com>
2020-03-19 20:55:00 +0400

    Updates

---
+++ precache-manifest.07173dd2badb289cd79e0042041902a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3c206b237e11c0e7d94f0f073f1f89",
+    "url": "/vkontacte_1/index.html"
+  },
+  {
+    "revision": "a0c1a213e3dd0d42b0c2",
+    "url": "/vkontacte_1/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "acbc11b5189df6525590",
+    "url": "/vkontacte_1/static/css/main.c47cf90e.chunk.css"
+  },
+  {
+    "revision": "a0c1a213e3dd0d42b0c2",
+    "url": "/vkontacte_1/static/js/2.62ac45a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vkontacte_1/static/js/2.62ac45a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbc11b5189df6525590",
+    "url": "/vkontacte_1/static/js/main.a7efa73a.chunk.js"
+  },
+  {
+    "revision": "2497f152c3bdf5afb9ac",
+    "url": "/vkontacte_1/static/js/runtime-main.9e38c4b9.js"
+  },
+  {
+    "revision": "6533e6d0d4f2e3889ddc926ba7225d2d",
+    "url": "/vkontacte_1/static/media/avatar.6533e6d0.png"
+  },
+  {
+    "revision": "1bab3ffd5beca8a9fd0bc2d332223ec4",
+    "url": "/vkontacte_1/static/media/icon.1bab3ffd.svg"
+  },
+  {
+    "revision": "9f87697c5bf42f7f975ea8e93c09f550",
+    "url": "/vkontacte_1/static/media/user_icon.9f87697c.svg"
+  },
+  {
+    "revision": "e25dda641158b9cb4763770161f82fd5",
+    "url": "/vkontacte_1/static/media/Акции в обложках.e25dda64.png"
+  },
+  {
+    "revision": "3d833e14f0f23e255373d19af452e648",
+    "url": "/vkontacte_1/static/media/Акции и скидки.3d833e14.png"
+  },
+  {
+    "revision": "ba3779d11b4267fac8afed79f7379cf5",
+    "url": "/vkontacte_1/static/media/Акционные товары.ba3779d1.png"
+  },
+  {
+    "revision": "e05095345744b6b3a91569c808ee8b20",
+    "url": "/vkontacte_1/static/media/Отзывы.e0509534.png"
+  },
+  {
+    "revision": "527308e8844ceeb5efc49d0d88908a09",
+    "url": "/vkontacte_1/static/media/Отзывы_1.527308e8.png"
+  },
+  {
+    "revision": "7e7aa5c676fd9c0dcbf4a461d171127f",
+    "url": "/vkontacte_1/static/media/Персональное предложение.7e7aa5c6.png"
+  },
+  {
+    "revision": "09538723e648d068b3cf43bcaa4ec2df",
+    "url": "/vkontacte_1/static/media/Рекомендованные товары.09538723.png"
+  }
+]);
